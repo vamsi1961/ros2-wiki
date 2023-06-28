321337dbc52e6db0e6b8d39027721362623eb08f
vamsi1961 <cheedellavamsikishore@gmail.com>
2023-06-28 20:01:42 +0530

    working

--- ros2_ws/src/my_py_pkg/my_py_pkg/my_first_node.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python3 
+# above is interpretor line.
+
+
 import rclpy
 from rclpy.node import Node
+# ti write node we import Node class
 
 
 def main(args =None):
     # implement ros2 communication main code
     rclpy.init(args=args)
+
 
     node = Node("py_test")
     node.get_logger().info("Hello ROS2")
